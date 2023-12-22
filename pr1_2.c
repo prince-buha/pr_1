f35a14cb5254f516ba425220c3608db20eae1e80
prince-buha <150029430+prince-buha@users.noreply.github.com>
2023-12-22 10:39:45 +0530

    Update pr1_2.c

--- pr1_2.c
+++ pr1_2.c
@@ -1,3 +1,16 @@
+/*
+Q.2 Write C Program to find gross salary by adding % of HRA, DA, and TA of user choice.
+For example,
+Input:
+Base Salary: 1000
+HRA=10%
+DA=5%
+TA=8%
+
+Output: 
+Rs. 1230
+	*/
+
 #include<stdio.h>
 
 
