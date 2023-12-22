77204eb4a3fdd2f80f07c71b8128e1199327b705
prince-buha <150029430+prince-buha@users.noreply.github.com>
2023-12-22 10:38:42 +0530

    Update pr1_2.c

--- pr1_2.c
+++ pr1_2.c
@@ -1,34 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    float basicSalary, hraPercent, daPercent, taPercent;
-    float hra, da, ta, grossSalary;
 
-    printf("Enter Basic Salary: ");
-    scanf("%f", &basicSalary);
+main(){
+	int bs;
+	int hrs,da,ta;
+	printf("enter your salary:-",bs);
+	scanf("%d",&bs);
+	
+	hrs = (bs*10)/100;
+	da = (bs*5)/100;
+	ta = (bs*8)/100;
+	
+	bs=hrs+da+ta+bs;
+	printf("answer is %d",bs);
 
-    printf("Enter percentage of House Rent Allowance (HRA): ");
-    scanf("%f", &hraPercent);
 
-    printf("Enter percentage of Dearness Allowance (DA): ");
-    scanf("%f", &daPercent);
 
-    printf("Enter percentage of Travel Allowance (TA): ");
-    scanf("%f", &taPercent);
-
-    hra = (hraPercent / 100.0) * basicSalary;
-    da = (daPercent / 100.0) * basicSalary;
-    ta = (taPercent / 100.0) * basicSalary;
-
-    
-    grossSalary = basicSalary + hra + da + ta;
-
-    
-    printf("\nBasic Salary: %.2f\n", basicSalary);
-    printf("House Rent Allowance (HRA): %.2f\n", hra);
-    printf("Dearness Allowance (DA): %.2f\n", da);
-    printf("Travel Allowance (TA): %.2f\n", ta);
-    printf("Gross Salary: %.2f\n", grossSalary);
-
-    return 0;
 }
+	
